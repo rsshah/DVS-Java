62fcf83be886ad5a785f25390c23820e2a5a1380
gta0004 <gta0004@gmail.com>
2014-10-28 22:51:11 -0700

    User Interface Implemented

--- dvs-core/src/main/java/com/ucsd/globalties/dvs/core/Main.java
+++ dvs-core/src/main/java/com/ucsd/globalties/dvs/core/Main.java
@@ -1,7 +1,19 @@
 package com.ucsd.globalties.dvs.core;
 
-public class Main {
+import com.ucsd.globalties.dvs.core.ui.LandingScene;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
   public static void main(String[] args) {
-    Patient.builder().gender("").birth("").build();
+    //Patient.builder().gender("").birth("").build();
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    LandingScene landingScene = new LandingScene(stage);
+    
   }
 }
--- dvs-core/src/main/java/com/ucsd/globalties/dvs/core/ui/LandingScene.java
+++ dvs-core/src/main/java/com/ucsd/globalties/dvs/core/ui/LandingScene.java
@@ -1,5 +1,67 @@
 package com.ucsd.globalties.dvs.core.ui;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class LandingScene {
+
+  public LandingScene(Stage stage) {
+    stage.setTitle("Digital Vision Screening");
+    GridPane grid = new GridPane();
+    grid.setAlignment(Pos.CENTER);
+    grid.setHgap(10);
+    grid.setVgap(10);
+    grid.setPadding(new Insets(25, 25, 25, 25));
+
+    Text scenetitle = new Text("Welcome");
+    scenetitle.setFont(Font.font("Calibri", FontWeight.NORMAL, 20));
+    grid.add(scenetitle, 0, 0, 2, 1);
+
+    Label userName = new Label("Patient Name:");
+    grid.add(userName, 0, 1);
+
+    TextField userTextField = new TextField();
+    grid.add(userTextField, 1, 1);
+
+    Label pw = new Label("School:");
+    grid.add(pw, 0, 2);
+
+    PasswordField pwBox = new PasswordField();
+    grid.add(pwBox, 1, 2);
+
+    Button btn = new Button("Next");
+    HBox hbBtn = new HBox(10);
+    hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+    hbBtn.getChildren().add(btn);
+    grid.add(hbBtn, 1, 4);
+
+    final Text actiontarget = new Text();
+    grid.add(actiontarget, 1, 6);
+
+    btn.setOnAction(new EventHandler<ActionEvent>() {
+
+        @Override
+        public void handle(ActionEvent e) {
+            //handle next event
+        }
+    });
+
+    Scene scene = new Scene(grid, 300, 275);
+    stage.setScene(scene);
+    stage.show();
+  }
 
 }
