424720cc0e8eb28da4b0ea1b0f5cd50f46fec3ea
Rahul <rss002@ucsd.edu>
2014-11-16 22:49:35 -0800

    use cropped pupil rect in pupil object ctor slight error checking TODO determine if rect is okay or if we should isolate the actual pupil circle

--- dvs-core/src/main/java/com/ucsd/globalties/dvs/core/Eye.java
+++ dvs-core/src/main/java/com/ucsd/globalties/dvs/core/Eye.java
@@ -18,67 +18,72 @@ import org.opencv.imgproc.Imgproc;
 
 @Slf4j
 public class Eye {
+  
   @Getter
   private Mat mat;
   private Pupil pupil;
-  
+
   public Eye(Mat mat) {
     this.mat = mat;
   }
-  
+
   public Pupil getPupil() {
     if (pupil == null) {
       pupil = findPupil();
     }
     return pupil;
   }
-  
+
   /**
-   * Algorithm from 
+   * Algorithm from
    * https://opencv-code.com/tutorials/pupil-detection-from-an-eye-image/
    * https://github.com/bsdnoobz/opencv-code/blob/master/pupil-detect.cpp
-   * TODO return a Mat with just the pupil, not including the eye
-   * TODO Error check in case multiple contours match conditions
+   * 
+   * TODO return a Mat with just the pupil, not including the eye 
+   * TODO Error check in case multiple contours match conditions 
    * TODO refine conditions to work for other pictures as well (if they do not already)
    */
   private Pupil findPupil() {
     int code = (new Random()).nextInt();
     Mat src = new Mat();
     Mat invt = new Mat();
-    /*Ideal image:
-    Mat source = Highgui.imread(new File(Main.class.getResource("/pics/ideal-image.jpg").getFile()).getAbsolutePath());
-    source.copyTo(src);
-    source.copyTo(invt);
-    */
     mat.copyTo(src);
     mat.copyTo(invt);
     Mat gray = new Mat();
-    Mat invertcolormatrix= new Mat(src.rows(),src.cols(), src.type(), new Scalar(255,255,255));
+    Mat invertcolormatrix = new Mat(src.rows(), src.cols(), src.type(), new Scalar(255, 255, 255));
     Core.subtract(invertcolormatrix, src, invt);
-    //Highgui.imwrite("mat-invt-"+code+".jpg", invt);
     Imgproc.cvtColor(invt, gray, Imgproc.COLOR_BGR2GRAY);
-    //Highgui.imwrite("mat-gray1-"+code+".jpg", gray);
-    //use 220,225 for ideal image
+    // use 220,225 for ideal image
     Imgproc.threshold(gray, gray, 175, 180, Imgproc.THRESH_BINARY);
-    //Highgui.imwrite("mat-gray2-"+code+".jpg", gray);
     List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
     Imgproc.findContours(gray.clone(), contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
-    Imgproc.drawContours(gray, contours, -1, new Scalar(255,255,255), -1);
-    //Highgui.imwrite("mat-gray3-"+code+".jpg", gray);
+    Imgproc.drawContours(gray, contours, -1, new Scalar(255, 255, 255), -1);
+    Mat pupilMat = null;
     for (int i = 0; i < contours.size(); i++) {
       Double area = Imgproc.contourArea(contours.get(i));
       Rect rect = Imgproc.boundingRect(contours.get(i));
-      int radius = rect.width/2;          
-      //Cond1: area of contour
-      //Cond2: compare rectangular area so it is as close as possible to a square. A rect that bounds a perfect circle is a perfect square
-      //Cond3: Compare circular contour area to perfect circular area
-      //Should refine last condition so it is usable condition, might be able to use to center point perfectly
-      if (area >= 1000 && Math.abs(1 - ((double)rect.width / (double)rect.height)) <= 0.2 /*Math.abs(1 - (area / (Math.PI * Math.pow(radius, 2)))) <= 0.2*/) {
-        log.info("Contour info: Area " + area + " first arg " + (Math.abs(1 - ((double)rect.width / (double)rect.height))) + " second arg " + Math.abs(1 - (area / (Math.PI * Math.pow(radius, 2)))));
-        Core.circle(src, new Point(rect.x + radius, rect.y + radius), radius, new Scalar(0,0,255),2);
+      int radius = rect.width / 2;
+      // Cond1: area of contour
+      // Cond2: compare rectangular area so it is as close as possible to a
+      // square. A rect that bounds a perfect circle is a perfect square
+      // Cond3: Compare circular contour area to perfect circular area
+      // Should refine last condition so it is usable condition, might be able
+      // to use to center point perfectly
+      double boundingArea = Math.abs(1 - ((double) rect.width / (double) rect.height));
+      if (area >= 1000 && boundingArea <= 0.2) {
+        double unk = Math.abs(1 - (area / (Math.PI * Math.pow(radius, 2))));
+        log.info("Contour info: Area " + area + " first arg " + boundingArea + " second arg " + unk);
+        if (pupilMat != null) {
+          log.warn("Setting pupilMat after a valid one had already been found.");
+        }
+        pupilMat = new Mat(src, rect);
       }
     }
-    Highgui.imwrite("pupil-"+code+".jpg", src);
-    return new Pupil(src);
+    if (pupilMat == null) {
+      log.error("Unable to find an adequate pupil");
+      throw new RuntimeException();
+    }
+    Highgui.imwrite("pupil-" + code + ".jpg", pupilMat);
+    return new Pupil(pupilMat);
   }
 }
