618665f5504b89256ee798ff2514020bdfe995ac
gta0004 <gta0004@gmail.com>
2014-12-08 20:48:51 -0800

    added compiled binaries for opencv so that jar files will work

--- dvs-core/src/main/java/com/ucsd/globalties/dvs/core/Main.java
+++ dvs-core/src/main/java/com/ucsd/globalties/dvs/core/Main.java
@@ -1,7 +1,13 @@
 package com.ucsd.globalties.dvs.core;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.Enumeration;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,12 +16,11 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
-import org.opencv.core.Core;
-
 @Slf4j
 public class Main extends Application {
   private static final String HAAR_FACE = "/haarcascade_frontalface_alt.xml";
   private static final String HAAR_EYE = "/haarcascade_eye.xml";
+  private static int BUFFER_SIZE = Short.MAX_VALUE;
   private static Controller controller;
   public static String HAAR_FACE_PATH;
   public static String HAAR_EYE_PATH;
@@ -28,12 +33,58 @@ public class Main extends Application {
   
   public static void loadLibraryComponents() {
     // load OpenCV constants
-    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    loadOpencvLibrary();
     //Workaround for "/" thing, load as file then get filepath
     File fp = new File(Main.class.getResource(HAAR_FACE).getFile());
     File ep = new File(Main.class.getResource(HAAR_EYE).getFile());
     HAAR_FACE_PATH = fp.getAbsolutePath();
     HAAR_EYE_PATH = ep.getAbsolutePath();
+  }
+  
+  private static void loadOpencvLibrary() {
+    try {
+      InputStream in = null;
+      File fileOut = null;
+      String osName = System.getProperty("os.name");
+      log.info(osName);
+      if(osName.startsWith("Windows")){
+        int bitness = Integer.parseInt(System.getProperty("sun.arch.data.model"));
+        if(bitness == 32){
+          log.info("32 bit detected");
+          in = Main.class.getResourceAsStream("/opencv/opencv_java249_x86.dll");
+          fileOut = File.createTempFile("lib", ".dll");
+        }
+        else if (bitness == 64){
+          log.info("64 bit detected");
+          in = Main.class.getResourceAsStream("/opencv/opencv_java249_x64.dll");
+          fileOut = File.createTempFile("lib", ".dll");
+        }
+        else{
+          log.info("Unknown bit detected - trying with 32 bit");
+          in = Main.class.getResourceAsStream("/opencv/opencv_java249_x86.dll");
+          fileOut = File.createTempFile("lib", ".dll");
+        }
+      }
+      else if(osName.equals("Mac OS X")){
+        log.info("Mac os detected");
+        in = Main.class.getResourceAsStream("/opencv/libopencv_java249.dylib");
+        fileOut = File.createTempFile("lib", ".dylib");
+      }
+
+      OutputStream out = new BufferedOutputStream(new FileOutputStream(fileOut), BUFFER_SIZE);
+      int b = 0;
+      
+      while ((b = in.read()) >= 0) {
+        out.write(b);
+      }
+      out.flush();
+      in.close();
+      out.close();
+      System.load(fileOut.toString());
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to load opencv native library", e);
+    }
   }
   
   public static Controller getController() {
--- dvs-core/src/main/java/com/ucsd/globalties/dvs/core/PupilDetectionTest.java
+++ dvs-core/src/main/java/com/ucsd/globalties/dvs/core/PupilDetectionTest.java
@@ -37,7 +37,6 @@ public class PupilDetectionTest {
   }
 
   public static void clean() {
-    Main.loadLibraryComponents();
     for (Iterator<Pair<String, String>> it = TEST_PAIRS.iterator(); it.hasNext();) {
       Pair<String, String> picPair = it.next();
       File fst = new File(PupilDetectionTest.class.getResource("/pics/" + picPair.getLeft()).getFile());
